c864dda1f90305b52cd2b8beed3a6f530590054e
mahidixit1221-lgtm <mahidixit1221@gmail.com>
2026-01-22 00:39:59 +0530

    commit

---
+++ Wrk.java
@@ -0,0 +1,9 @@
+public class Wrk{
+    public static void main(String[] args){
+        int y = 10;
+        System.out.println("HelloWorld");
+        System.out.print(args[0]);
+		System.out.print( "\t" + args[0] + args[1]);
+
+    }
+}
